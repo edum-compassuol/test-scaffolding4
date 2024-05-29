0db7b286c6ace18894993a7608b51ed9b00976c9
eduardomallmann <mallmann.edu@gmail.com>
2024-05-29 15:30:26 -0300

    Add initial structure

---
+++ src/main/java/com/example/test/scaffolding/TestScaffolding4Application.java
@@ -0,0 +1,13 @@
+package com.example.test.scaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestScaffolding4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestScaffolding4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test/scaffolding/TestScaffolding4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test.scaffolding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestScaffolding4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
